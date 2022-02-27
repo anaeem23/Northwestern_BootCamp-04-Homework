f4b0d848be125bcce3979dab71bb0e273e09194d
Abeer Naeem <abeernaeem6@gmail.com>
2022-02-26 19:06:11 -0600

    Cleaned up code and added more quiz features

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
+// Get all variable from HTML file
+
 var startbtn = document.querySelector("#startGame");
 var questionPlacement = document.querySelector("#questionPlacement");
 var ans1Placement = document.querySelector("#Ans1Placement");
 var ans2Placement = document.querySelector("#Ans2Placement");
 var ans3Placement = document.querySelector("#Ans3Placement");
 var ans4Placement = document.querySelector("#Ans4Placement");
+var ansChoice = document.querySelector("#ansChoice");
+var rightWrong = document.querySelector("#rightWrong");
+var playAgain = document.querySelector("#playAgain");
+
+// Create the Questions and Answers
 
 var questions = ["Test1", "Test2", "Test3", "Test4"];
 
@@ -14,32 +21,71 @@ var ans4 = ["ANS4", "ANS4", "ANS4", "ANS4"];
 
 var answers = [ans1, ans2, ans3, ans4];
 
-var i = 0;
+var correctAnswers = ["#Ans3Placement","#Ans1Placement","#Ans2Placement","#Ans4Placement"]
 
-startbtn.addEventListener("click", increaseI);
+// Function to control questions
 
-function increaseI() {
+var i = 0; 
 
-    startbtn.textContent = "Next";
+function changeQuestion() {
 
+    startbtn.style.display = "none"
 
+    if ( i < questions.length) {
 
-  if (i >= questions.length) {
-    console.log("test");
+        questionPlacement.textContent = questions[i];
 
-    startbtn.textContent = "Play Again?"
+        var ans = answers[i];
+    
+        ans1Placement.textContent = ans[0];
+        ans2Placement.textContent = ans[1];
+        ans3Placement.textContent = ans[2];
+        ans4Placement.textContent = ans[3];
+    
+        i++;
 
-    i = 0;
-  } else {
-    questionPlacement.textContent = questions[i];
+    }else {
 
-    var ans = answers[i];
-
-    ans1Placement.textContent = ans[0];
-    ans2Placement.textContent = ans[1];
-    ans3Placement.textContent = ans[2];
-    ans4Placement.textContent = ans[3];
-
-    i++;
-  }
+        window.open("scorePage.html","_self")
+        
+    }
 }
+
+// Function to show if answer was right
+
+function right() {
+    rightWrong.textContent = "Correct!"
+}
+
+// function to show if answer was wrong 
+
+function wrong () {
+    rightWrong.textContent = "Wrong!"
+}
+
+
+// Continute game with Ans Choices
+
+ansChoice.addEventListener("click", function(event) {
+
+    if (i>0) {
+        changeQuestion()    
+        if (event.target.matches(correctAnswers[i-2])) {
+            right()
+        }else {
+            wrong()
+        }
+    }
+   
+})
+
+
+
+//Start the game
+
+startbtn.addEventListener("click", changeQuestion);
+
+
+
+
+
